ca15ee0c7e9647c19e4eab755734ed1cce22a4b9
Jürgen Fast <j.fast@micromata.de>
2017-03-07 10:35:58 +0100

    chapter5

---
+++ chapter5/https-with-spring-boot/src/main/java/de/micromata/spring/security/example/security/conf/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package de.micromata.spring.security.example.security.conf;
+
+import de.micromata.spring.security.example.security.user.AuthenticatedUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * @author Jürgen Fast - j.fast@micromata.de
+ */
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = AuthenticatedUserService.class)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/noSecurity").permitAll()
+            .anyRequest().authenticated()
+            .and().formLogin().loginPage("/login").permitAll()
+            .and().logout().permitAll();
+
+        http.csrf().ignoringAntMatchers("/console/**")
+            .and().headers().frameOptions().sameOrigin();
+    }
+
+    @Autowired
+    public void globalSecurityConfiguration(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+}
